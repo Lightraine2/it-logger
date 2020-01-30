d7e186d39e44530a4fd0fdf0c60bf9fde0f3fd2d
Laurence Dale <laurencedale1986@gmail.com>
2020-01-30 22:16:17 +0000

    77 search bar and logs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
+import SearchBar from '../src/components/layout/SearchBar';
+import Logs from '../src/components/logs/Logs';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -10,9 +13,12 @@ const App = () => {
   });
   
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+        <SearchBar />
+        <div className="container">
+          <Logs />
+        </div>
+    </Fragment>
   );
 }
 
