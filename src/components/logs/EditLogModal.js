311394532c0f09c2367c77426b2307d4356858c2
Laurence Dale <laurencedale1986@gmail.com>
2020-02-05 21:10:01 +0000

    80 - finish app UI

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const EditLogModal = () => {
     const [message, setMessage] = useState('');
     const [attention, setAttention] = useState(false);
     const [tech, setTech] = useState('');
@@ -15,11 +15,13 @@ const AddLogModal = () => {
         setTech('');
         setAttention(false);
         }
+
+        
     }
 
 
     return (
-        <div id="add-log-modal" className="modal" style={modalStyle}>
+        <div id="edit-log-modal" className="modal" style={modalStyle}>
             <div className="modal-content">
                 <h4>Enter System Log</h4>
                 <div className="row"> 
@@ -71,4 +73,4 @@ const modalStyle = {
     height: '75%'
 };
 
-export default AddLogModal;
+export default EditLogModal;
