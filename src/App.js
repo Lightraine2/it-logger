7550cc51f3357c91ee2e93c3c1a412c8e95daf68
Lightraine2 <laurencedale1986@gmail.com>
2020-02-09 16:01:08 +0000

    81 - redux store and provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useEffect} from 'react';
+import React, { Fragment, useEffect } from 'react';
 import SearchBar from '../src/components/layout/SearchBar';
 import Logs from '../src/components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
@@ -6,19 +6,22 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
-  useEffect( ()=> {
+  useEffect(() => {
     // Init Materialize js
     M.AutoInit();
   });
-  
+
   return (
-    <Fragment>
+    <Provider store={store}>
+      <Fragment>
         <SearchBar />
         <div className="container">
           <AddBtn />
@@ -28,7 +31,8 @@ const App = () => {
           <TechListModal />
           <Logs />
         </div>
-    </Fragment>
+      </Fragment>
+    </Provider>
   );
 }
 
