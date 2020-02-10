aa87d059073543d4e618033f6d8bd7bc685f569a
Laurence Dale <laurencedale1986@gmail.com>
2020-02-10 20:42:31 +0000

    89 - tech options selector

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import { Connect, connect } from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import {addLog} from '../../actions/logActions';
+import TechSelectOptions from '../techs/TechSelectOptions';
 
 
 const AddLogModal = ({ addLog }) => {
@@ -53,9 +54,8 @@ const AddLogModal = ({ addLog }) => {
                             e.target.value
                         )}>
                            <option value="" disabled>Select Technician</option>
-                           <option value="John Doe">John Doe</option>
-                           <option value="Sam Smith">Sam Smith</option>
-                           <option value="Sara Wilson">Sara Wilson</option>
+
+                           <TechSelectOptions />
                          </select>
                     </div>
                 </div>
